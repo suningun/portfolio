a5a58bb75a0bbda7b82e2fb606e8f5bc94f49493
suningun <suningun55555@gmail.com>
2026-09-19 10:59:30 +0700

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  base: "/portfolio/",
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
